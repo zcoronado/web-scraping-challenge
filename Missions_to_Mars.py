864fb6521c7219ed67412f3f4fd3f0bb063f04c7
Zaira Coronado <coronado.zaira@gmail.com>
2021-09-07 16:29:10 -0700

    added py script

---
+++ Missions_to_Mars.py
@@ -0,0 +1,49 @@
+from splinter import Browser
+import pandas as pd
+from webdriver_manager.chrome import ChromeDriverManager
+executable_path = {'executable_path': ChromeDriverManager().install()}
+
+def scrape():
+    browser = Browser('chrome', **executable_path, headless=False)
+    title, paragrah = news(browser)
+    data = {}
+    data['title'] = title
+    data['paragrah'] = paragrah
+    data['image'] = image(browser)
+    data['facts'] = facts() 
+    data['hemis'] = hemis(browser)
+
+    return data 
+
+def news(browser):
+    browser.visit('https://redplanetscience.com/')
+    title = browser.find_by_css('div.content_title').text
+    paragrah = browser.find_by_css('div.article_teaser_body').text
+    return title, paragrah
+
+def image(browser):
+    browser.visit('https://spaceimages-mars.com/')
+    return browser.find_by_css('img.headerimage')['src']
+
+def facts():
+    return pd.read_html('https://galaxyfacts-mars.com/')[0].rename(columns ={ 0 : 'Index', 1 : 'Mars', 2 : 'Earth'}).to_html()
+
+def hemis(browser): 
+    browser.visit('https://marshemispheres.com/')
+
+    hemispheres = []
+    for i in range(4):
+        hemisphere = {}
+        hemisphere['title'] = browser.find_by_css("a.itemLink h3")[i].text
+        browser.find_by_css("a.itemLink h3")[i].click()
+        hemisphere['url'] = browser.find_by_text("Sample")["href"]
+        hemispheres.append(hemisphere)
+        browser.back()
+    browser.quit()
+    return hemispheres
+
+
+
+
+
+
