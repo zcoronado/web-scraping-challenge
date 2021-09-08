e914661eeded61ffec6b358ffe1f55ef4a7c9a20
Zaira Coronado <coronado.zaira@gmail.com>
2021-09-07 22:48:23 -0700

    app.py

---
+++ app.py
@@ -0,0 +1,45 @@
+from scrape import scrape
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
+
+#create an instance of flask 
+app = Flask(__name__)
+
+#connect to Mongo
+mongo = PyMongo(app, uri='mongodb://localhost:27017/mars_db')
+
+#route to index.html 
+app.route("/")
+def web():
+    # mars = mongo.db.mars.find_one()
+    # return render_template('index.html', mars=mars)
+    
+    # Find one record of data from mongo db
+    mars = mongo.db.collection.find_one()
+
+    print(mars_data)
+
+    # Return template and data
+    return render_template("index.html", mars=mars)
+
+# Route that will trigger the scrape function
+@app.route("/scrape")
+
+def scrape():
+
+    # Run the scrape function
+    mars_dict = scrape.scrape_info()
+
+    # Update the Mongo database using update and upsert=True
+    mongo.db.collection.update({}, mars_dict, upsert=True)
+
+    # Redirect back to home page
+    return redirect("/")
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+
+# if '__main__'==__name__:
+#     app.run(debug=True)
